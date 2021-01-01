e509d77252d4d2b18f0f316de0f12b0d298627c7
topDevoloper <jichangho20@gmail.com>
2021-01-01 03:44:51 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,15 @@
+class Main {
+  public static void main(String[] args) {
+    // N 원. 단, N은 항상 10의 배수.
+    int n = 1260;
+    int count = 0;
+
+    int[] coinTypes = { 500, 100, 50, 10 }; // 큰 화폐부터 차례로 확인.
+    for (int coin : coinTypes) {
+      count += n;
+      n %= coin;
+    }
+
+    System.out.println(count);
+  }
+}
